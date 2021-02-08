069609051dfa2027206ebfdc3ab6e0bb52858e42
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2021-02-08 11:50:08 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '244c89fa6e67a0e12da0712387477449'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '08576b19-c404-4cca-bd3d-8ed5402214c1'
+};
