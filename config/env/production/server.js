7cca30d7c88e54ef7cb702bfba61e74b3a57ec86
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2021-02-10 12:36:38 +0100

    Update database config

---
+++ config/env/production/server.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+    url: env('postgres://uuhakpdamigspn:9ea1a3373576ce8b695a8a152f1da4848430a7e6862c11aab49a7029a5ed3720@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/ddhsu2qvvalocc'),
+});
