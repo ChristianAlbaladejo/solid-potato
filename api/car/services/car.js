a875e847f6d4fd92fa41445c98e6a3a335f23434
ChristianAlbaldejo <christian.albaladejo06@gmail.com>
2021-02-12 12:43:31 +0100

    Updates to users

--- api/car/services/car.js
+++ api/car/services/car.js
@@ -6,8 +6,4 @@
  */
 
 module.exports = {
-    async publish(){
-        const products = await strapi.services.car.find( {"kilometros": 50} );
-        return products.map((products, i) => `${i}. ${products}`);
-    }
 };
